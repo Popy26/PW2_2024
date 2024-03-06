3fd19727d0fcf66041db168d39d28813a39e5fb1
popy26 <popyannisa365@gmail.com>
2024-03-06 11:14:59 +0700

    apload

---
+++ pertemuan3/pratikum1/libfungsi.php
@@ -0,0 +1,16 @@
+<?php
+  
+  function hitungluas($alas,$tinggi){
+    $luas = $alas * $tinggi / 2;
+    return $luas;
+  }
+  function deteksiluas($alas, $tinggi){
+    if($alas == $tinggi){
+        return "segitiga sama sisi";
+    }elseif ($alas == 0.5 * $tinggi || $tinggi == 0.5 * $alas){
+        return "segitiga siku-siku";
+    }else{
+        return"segitiga sembarang";
+    }
+}
+?>
